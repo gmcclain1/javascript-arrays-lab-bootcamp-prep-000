6e350137af7ca1ac6eba3eec27e263a1ebacaf05
Jassen McClain <new.coder@aol.com>
2018-01-31 04:00:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+  return kittens;
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+  return kittens;
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+function appendKitten(name){
+  return[...kittens, "Broom"];
+}
